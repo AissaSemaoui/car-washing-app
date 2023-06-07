5e6fab672da44a91063a1c3612ee80a3e1167bca
AissaSemaoui <semaoui0347@gmail.com>
2023-06-07 13:16:27 +0300

    fix origin

--- middleware.js
+++ middleware.js
@@ -1,24 +1,7 @@
 import { NextResponse } from "next/server";
-import NextCors from "nextjs-cors";
 
-export async function middleware(request) {
-  const response = NextResponse.next();
-
-  if (request.nextUrl.pathname.startsWith("/api")) {
-    response.headers.append("Access-Control-Allow-Origin", "*");
-    response.headers.append("Access-Control-Allow-Methods", "*");
-    response.headers.append("Access-Control-Allow-Headers", "*");
-    response.headers.append("Access-Control-Allow-Credentials", "true");
-    response.headers.append("Access-Control-Max-Age", "86400");
-  }
-
+export function middleware(request) {
   if (request.method === "OPTIONS") {
-    await NextCors(req, res, {
-      // Options
-      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-      origin: "*",
-      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    });
     return new NextResponse(null, {
       status: 200,
       headers: {
@@ -30,6 +13,4 @@ export async function middleware(request) {
       },
     });
   }
-
-  return response;
 }
