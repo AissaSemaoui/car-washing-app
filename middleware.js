ec76b062fc9c560bb4e51e00ee24d6220e0aace7
AissaSemaoui <semaoui0347@gmail.com>
2023-06-07 12:49:17 +0300

    fixing cors

--- middleware.js
+++ middleware.js
@@ -1,7 +1,14 @@
 import { NextResponse } from "next/server";
+import NextCors from "nextjs-cors";
 
-export function middleware(request) {
+export async function middleware(request) {
   if (request.method === "OPTIONS") {
+    await NextCors(req, res, {
+      // Options
+      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+      origin: "*",
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
     return new NextResponse(null, {
       status: 200,
     });
